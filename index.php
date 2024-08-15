496328ad6a005fc8882e4ac9238402197024d515
dhea <dewantidea76@gmail.com>
2024-08-15 11:57:54 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+//echo 'Nama : ';
+//echo 'Dhea dewanti ';
+
+//$nama ="dheaaa ";
+//echo "Nama : ";
+//echo 'Dhea $nama Dewanti';
+//echo "\n"; 
+
+
+//Tipe Data Boolean 
+//echo "Benar";
+//echo true;
+
+//echo "Salah ";
+//echo false;
+
+
+//Variable di php 
+$nama = "Dhea ";
+$umur = "17";
+$ttl  = "Sumedang 29 September 2006";
+$Askot = "Sumedang ";
+
+echo " ";
+echo "Name : $nama" . "<br>";
+echo "umur : $umur" . "<br>";
+echo "ttl  : $ttl" . " <br> ";
+echo "Askot: $Askot" . "<br>";
